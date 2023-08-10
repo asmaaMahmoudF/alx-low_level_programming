7d355b2e3bbc5b6f973f0b782e98a6a014790598
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-10 14:21:56 -0700

    4-puts.c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 int main(void)
        /**
 	* main - Entry point
-	* Return : Always 0 (Success)
+	* * Return : Always 0 (Success)
 	*/	
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);}
+return (0);
+}
