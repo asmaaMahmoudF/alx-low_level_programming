e81443ad4daa5caa48cd45f5e9ed527f945ca581
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-10 13:44:48 -0700

    4-puts.c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-gcc -E $CFILE > cisfun
+#includ<stdio.h>
+int main(void)
+       /*
+	* This is an entry point
+	*/	
+{
+puts("Programming is like building a multilingual puzzle");
+return(0);
+}
