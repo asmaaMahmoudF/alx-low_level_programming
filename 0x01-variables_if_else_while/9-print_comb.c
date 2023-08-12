17b822bd1ea46293cad892193ca364076e015b83
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-11 17:50:06 -0700

    9-print_comb.c task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: (0) success
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + 48);
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+digit++;
+}
+putchar('\n');
+return (0);
+}
