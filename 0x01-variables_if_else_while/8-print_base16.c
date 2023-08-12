930d60dd726bf07c2c22ed809d7779c1d0afad26
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-11 17:35:22 -0700

    8-print_base16.c task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: (0) success
+*/
+int main(void)
+{
+int digit = 48;
+while (digit <= 102)
+{
+putchar(ch);
+if (digit == 57)
+digit += 39;
+digit++;
+}
+putchar('\n');
+return (0);
+}
