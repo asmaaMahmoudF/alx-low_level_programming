803bbc76afd017251f95dafe2e632ebd1577df40
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-11 17:07:15 -0700

    4-print_alphabt.c task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: (0) success
+*/
+int main(void)
+{
+char ch = 'a';
+char CH = 'A';
+while (ch <= 'z')
+{
+putchar(ch);
+if (ch == 'e' || ch == 'q')
+ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
