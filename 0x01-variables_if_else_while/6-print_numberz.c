1b49e9c7940430284bccc3851a54d274540de055
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-11 17:20:51 -0700

    6-print_numberz.c task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: (0) success
+*/
+int main(void)
+{
+int ch = 48;
+while (ch <= 57)
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
