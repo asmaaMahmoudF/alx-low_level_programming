8732d664854a320069974cd7d2af34429a9c1516
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-15 16:52:24 -0700

    3-islower.c task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function that will print the alphapet 10 times
-*/
-void print_alphabet_x10(void)
-{
-int i, ch;
-for (ch = 0 ; ch <= 9 ; ch++)
-{
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-}
-}
-#include "main.h"
-/**
  *_islower - function to check if character is lowercase
  *@c : check input of function
  *return : returns 1 if `c` is lowercase otherwise always 0 (Success)
