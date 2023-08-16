eabb034c65295231c813c6d79900637a04aee6e3
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-15 19:43:27 -0700

    11-print_to_98.c task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * Description: print_to_98 - prints n to 98 counts
+ * separated by comma, followed by space and number
+ * should be printed in oreder
+ * @n: input
+ * Return void
+*/
+void print_to_98(int n)
+{
+int sum;
+if (sum > 98)
+{
+for (sum = n ; sum > 98 ; sum--)
+{
+printf("%d, ", count);
+}
+}
+else
+{
+for (sum = n ; sum < 98 ; sum++)
+{
+printf("%d, ", count);
+}
+}
+print("98\n");
+}
