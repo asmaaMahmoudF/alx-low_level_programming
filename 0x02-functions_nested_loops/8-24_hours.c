2587e9439a5513bff59592165cf1008ea8169d43
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-15 20:03:55 -0700

    ut

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
-* jack_bauer the entry point of file 
+* jack_bauer - entry point
+*
 * Description: prints every minute of the day
+*
 * Return: void
 */
 void jack_bauer(void)
