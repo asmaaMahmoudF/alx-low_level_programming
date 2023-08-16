e5486e40803e5158cf1ac0e1b1c05f42f59722a7
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-16 06:53:35 -0700

    100-times_table.c task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,9 @@ _putchar((prod / 100) + 48);/*get the first digit */
 _putchar((prod / 100) % 10 + 48);
 }
 else if (prod <= 99 && prod >= 10)
+{
 _putchar((prod / 10) + 48);/*get the second digit */
+}
 _putchar((prod % 10) + 48);
 }
 _putchar('\n');
