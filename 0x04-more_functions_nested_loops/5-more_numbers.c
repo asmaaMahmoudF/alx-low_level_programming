3f58bc81dec7053b94bfd3bc0ecc048e98c95e91
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-17 08:52:00 -0700

    5-more_numbers.c task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,19 @@
 
 void more_numbers(void)
 {
-int i;
+int i, j, num;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-printf("%d", j);
+num = j;
+if (j > 9)
+{
+_putchar(49);
+num = j % 10;
 }
-_putchar('\n');
+_putchar(num + 48);
+}
 }
 _putchar('\n');
 }
