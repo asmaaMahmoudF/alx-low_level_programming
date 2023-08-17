7fea57814f820d7f505b1a922875053db4e3850c
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-17 11:56:32 -0700

    8-print_square.c task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,11 +14,11 @@ if (size <= 0)
 {
 _putchar('\n');
 }
-else if ( size > 0)
+else if (size > 0)
 {
-for (i = 1; i <= size; i++)
+for (i = 0; i <= size; i++)
 {
-for (j = 1; j <= size; j++)
+for (j = 0; j <= size; j++)
 _putchar('#');
 _putchar('\n');
 }
