25659364e6e3d353931fac55c3953621d89449ed
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-17 10:30:19 -0700

    7-print_diagonal.c task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,17 +11,13 @@ void print_diagonal(int n)
 {
 int cal, row;
 if (n <= 0)
-{
 _putchar('\n');
-}
 else if (n > 0)
 {
 for (cal = 1; cal <= n; cal++)
 {
 for (row = 1; row <= cal; row++)
-{
 _putchar(32);
-}
 _putchar(92);
 _putchar('\n');
 }
