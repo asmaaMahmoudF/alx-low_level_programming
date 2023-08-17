b227254908da4d04d0a9bb4eca163982fd62a567
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-17 07:17:23 -0700

    mul.c task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -Entry point
+* Description: mul of two digits
+*
+* @a: input
+* @b: input
+* Return: multiplication of two digits
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
