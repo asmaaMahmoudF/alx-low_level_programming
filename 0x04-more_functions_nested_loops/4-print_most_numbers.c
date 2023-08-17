b7ef8abcee52482da26ab138c6ad711e8fffd2c0
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-17 07:53:35 -0700

    4-print_most_numbers.c task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -Entry point
+* Description: print_most_numbersprints numbers
+* except 2 & 4
+*
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+if (i == 2 || i == 4)
+i++;
+_putchar(i);
+}
+_putchar('\n');
+}
