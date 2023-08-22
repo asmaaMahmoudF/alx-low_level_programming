90073d79755e1f94a5fce2392c1f685e3b758449
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-21 19:16:32 -0700

    7-puts_half.c task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -20,3 +20,25 @@ for (i = i / 2; *(str + i) != '\0' ; i++)
 _putchar(*(str + i));
 _putchar('\n');
 }
+#include "main.h"
+
+/**
+* puts_half -Entry point
+* Description: puts_half prints half of a string followed by a new line.
+*
+* @str: input parameter
+*
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+i++;
+i++;
+
+for (i = i / 2; *(str + i) != '\0' ; i++)
+_putchar(*(str + i));
+_putchar('\n');
+}
