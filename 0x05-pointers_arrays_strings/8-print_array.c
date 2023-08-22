d73e39be8d9218c3d6abc12ee344f0c076bbfcac
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-21 19:59:20 -0700

    jjf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,22 +15,19 @@ void print_array(int *a, int n)
 int i = 0;
 while (i < n)
 {
-if (i != (n - 1))
+if (i < (n - 1))
 {
 printf("%d, ", a[i]);
-_putchar('\n');
 }
 else
 {
-printf("%d", a[i]);
-_putchar('\n');
+printf("%d\n", a[i]);
 }
 i++;
-if (n  == 0)
-{
-printf("\n");
-printf("\n");
-_putchar('\n');
 }
+if (n == 0)
+{
+_putchar('\n');
+_putchar('\n');
 }
 }
