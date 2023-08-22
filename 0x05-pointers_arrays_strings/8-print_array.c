7faa10da5b6b5ae1a6ee7a2b8990c2af3b6f151e
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-21 18:48:53 -0700

    8-print_array.c task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_array -Entry point
+* Description: print_array prints n elements of
+* an array of integers, followed by a new
+*
+* @a: input parameter
+* @n: input
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+int i = 0;
+while (i < n)
+{
+i++;
+if (i != (n - 1))
+printf("%d, ", a[i]);
+else
+printf("%d, ", a[i]);
+}
+_putchar('\n');
+}
