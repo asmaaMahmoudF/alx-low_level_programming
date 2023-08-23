4fa432432affd744ed45ed8529d2a661f01a8ba7
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-23 15:56:44 -0700

    5-rev_string.c task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* rev_string -Entry point
+* Description: rev_string returns the length of a string
+*
+* @s: input
+*
+* Return: void
+*/
+void rev_string(char *s)
+{
+int i, j, tmp;
+for (i = 0; s[i] = '\0'; i++)
+	;
+for (j = 0; j < i / 2; i++)
+{
+tmp = s[j];
+s[j] = s[j - 1 - i];
+s[j - 1 - i] = tmp;
+}
+}
