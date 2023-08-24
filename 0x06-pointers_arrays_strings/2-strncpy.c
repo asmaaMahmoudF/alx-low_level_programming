e353c31777ccf52b3be5daf8710918d25bf5f603
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-24 11:24:25 -0700

    2-strncpy.c task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strncpy -  concatenates two strings
+* @dest: input
+* @src: input
+* @n: input
+* Return: char
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int c, c2;
+c = 0;
+while (dest[c] != '\0')
+c++;
+for (c2 = 0; c2 < n &&  src[c2] != '\0' ; c2++)
+{
+dest[c] = src[c2];
+c++;
+}
+c2++;
+dest[c + c2] = '\0';
+return (dest);
+}
