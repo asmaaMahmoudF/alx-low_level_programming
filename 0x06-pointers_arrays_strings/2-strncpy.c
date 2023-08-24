95b5c00f421f0ab50d0b5e57649d9477ead287d1
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-24 11:57:49 -0700

    2-strncpy.c task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int c, c2;
 c = 0;
-while (dest[c] != '\0')
-c++;
-for (c2 = 0; c2 < n &&  src[c2] != '\0' ; c2++)
+for (c2 = 0; c2 < n && src[c2] != '\0'; c2++)
 {
-dest[c] = src[c2];
-c++;
+dest[c2] = src[c2];
 }
+while (c2 < n)
+{
+dest[c2] = '\0';
 c2++;
-dest[c + c2] = '\0';
+}
 return (dest);
 }
