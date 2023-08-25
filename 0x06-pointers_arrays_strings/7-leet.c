138ad00bd650877dc4e60dd7b71f84840f7745a8
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-24 18:35:36 -0700

    7-leet.c task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,20 +6,20 @@
 */
 char *leet(char *s)
 {
-char *cp = c;
+char *cp = s;
 char key[] = {'A', 'E', 'O', 'T', 'L'};
 int value[] = {4, 3, 0, 7, 1};
 unsigned int i;
 while (*s)
 {
-for (i = 0; i < sizeof(key) / sizeof(char) ; i++)
+for (i = 0; i < sizeof(key) / sizeof(char); i++)
 {
 if (*s == key[i] || *s == key[i] + 32)
 {
 *s = 48 + value[i];
 }
 }
-c++;
+s++;
 }
 return (cp);
 }
