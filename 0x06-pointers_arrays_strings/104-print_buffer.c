9cef81e7669daa862f9353229552b1d6615945c0
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-25 19:13:43 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,84 +1,77 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
-* isPrintTableASCII - prints a buffer
-* @n: input
-*
-* Return: 1 if true of 0 false
-*/
-
-int isPrintTableASCII(int n)
+ * isPrintableASCII - determines if n is a printable ASCII char
+ * @n: integer
+ * Return: 1 if true, 0 if false
+ */
+int isPrintableASCII(int n)
 {
-return (n >= 32 && n <= 126);
+	return (n >= 32 && n <= 126);
 }
-
 /**
-* print_Hexes - prints a buffer
-* @b: input
-* @start: input
-* @end: input
-*
-* Return: void
-*/
-void print_Hexes(char *b, int start, int end)
+ * printHexes - print hex values for string b in formatted form
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printHexes(char *b, int start, int end)
 {
-int i = 0;
-while (i < 10)
-{
-if (i < end)
-printf("%02x", *(b + start + end));
-else
-printf("  ");
-if (i % 2)
-printf(" ");
-i++;
-}
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
 }
 
 /**
-* print_ASCII - prints a buffer
-* @b: input
-* @start: input
-* @end: input
-*
-* Return: void
-*/
+ * printASCII - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
 
-void print_ASCII(char *b, int start, int end)
-{
-int ch, i = 0;
-while (i < end)
-{
-ch = *(b + i + start)
-if (!isPrintTableASCII(ch))
-ch = 46;
-printf("%c", ch);
-i++;
-}
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
 }
 
 /**
-* print_buffer - prints a buffer
-* @b: input
-* @size: input
-*
-* Return: void
-*/
+ * print_buffer - prints a buffer
+ * @b: string
+ * @size: size of buffer
+ */
 void print_buffer(char *b, int size)
 {
-int start, end;
-if (size > 0)
-{
-for (start = 0; start < size ; start += 10)
-{
-end = (size - start < 10) ? size - start : 10;
-printf("%08x: ", start);
-print_Hexes(b, start, end);
-print_ASCII(b, start, end);
-printf("\n");
-}
-}
-else
-printf("\n");
+	int start, end;
+
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+	}
+	else
+		printf("\n");
 }
