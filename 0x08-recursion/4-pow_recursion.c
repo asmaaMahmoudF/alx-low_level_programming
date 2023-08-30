5006c3c6b1626a1adf4746276c0819d131f7a9fb
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-30 13:55:18 -0700

    3-factorial.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _pow_recursion - returns the value of x raised to the power of y
+* @x : input
+* @y : input
+* Return: int
+*/
+int _pow_recursion(int x, int y)
+{
+int ntr = 0;
+if (*s != '\0')
+{
+ntr += _strlen_recursion(s + 1) + 1;
+}
+return (ntr);
+}
