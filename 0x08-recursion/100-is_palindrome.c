38d4d658b175541d424bfccda7b9ef3093cdd5c5
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-30 16:30:40 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,9 +10,10 @@ int _strlen_recursion(char *s)
 int ntr = 0;
 if (*s != '\0')
 {
-ntr += _strlen_recursion(s + 1) + 1;
+s++;
+return (_strlen_recursion(s) + 1);
 }
-return (ntr);
+return (0);
 }
 
 
