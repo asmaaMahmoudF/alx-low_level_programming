c2c7e3a6b39d2c5d33c61ba8720b1db717d32f11
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-08-30 17:33:31 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,55 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* checker - returns the natural square root of a number
-* @n : input
-* @rm : input
+* search_other -  concatenates two strings
+* @s: input
+* @str: input
+* @counter: input
 * Return: int
 */
-int checker(int n, int rm)
+int search_other(char s, char *str, int *counter)
 {
-if (rm == n)
-	return (1);
-if (!(n % rm))
-return (0);
-
-return (checker(n, rm + 1));
+	if (s == *str)
+		return (1);
+	else if (*str == '\0')
+		return (0);
+	else if (s == '*')
+		return (0);
+	else
+	{
+		*counter += 1;
+		return (search_other(s, ++str, &*counter));
+	}
 }
 /**
-* is_prime_number - returns 1 if the input integer
-* is a prime number, otherwise return 0
-* @n : input
+* wildcmp - compares two strings and returns 1
+* if the strings can be considered identical, otherwise return 0.
+* @s1: input
+* @s2: input
 * Return: int
 */
 int wildcmp(char *s1, char *s2)
 {
-if (n < 2)
-	return (0);
-return (checker(n, 2));
+	int counter;
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 == *s2)
+		return (wildcmp(++s1, ++s2));
+	else if (*s1 == '*')
+	{
+		counter = 0;
+		search_other(*++s1, s2, &counter);
+		s2 += counter;
+		return (wildcmp(s1++, s2++));
+	}
+	else if (*s2 == '*')
+	{
+		counter = 0;
+		search_other(*++s2, s1, &counter);
+		s1 += counter;
+		return (wildcmp(s1++, s2++));
+	}
+	else
+		return (0);
 }
