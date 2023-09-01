fe32693778b2d4d547bfdb01c44c8b58d2771aca
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-09-01 15:45:33 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main -  entry  point
+* @argc: input
+* @arg: input
+* Return: 0
+*/
+
+int main(int argc, char const *arg[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
