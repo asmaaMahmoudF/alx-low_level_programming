aef8e0013fe349358f98041392b06f7a18ad16fc
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-09-01 17:36:09 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,13 +14,10 @@ char *c;
 while (--argc)
 {
 for (c = argv[argc]; *c; c++)
-{
 if (*c < '0' || *c > '9')
-{
 return (printf("Error\n"), 1);
 sum += atoi(argv[argc]);
-}
-}
+
 }
 printf("%d\n", sum);
 return (0);
