8b8199e9f79649ecda538ad54a40d7a1d9fea3cc
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-09-11 14:44:29 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef _FUUNCTION_LIKE_MACRO_H
 #define _FUUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) > 0  ? (x):(-(x)))
-#endif
+#define ABS(x) ((x) > 0 ? (x) : (-(x)))
+#endif /* F_FUUNCTION_LIKE_MACRO_H */
