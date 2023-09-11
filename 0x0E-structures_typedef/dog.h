009d9b8d99f13b44ba129c9bd10e53cb17bf3ba6
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-09-11 15:05:24 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog -  writes the character c to stdout
+* @name : dog's anme
+* @age : dog's age
+* @owner : omar's name
+* Description : just a dog struct
+*/
+
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
