9237aea6fb061ed4e848fae00f6fd2ca381c06f8
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-09-13 14:05:38 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* array_iterator - that prints a name.
+* @array: input
+* @size: input
+* @action: the printing function pointer
+* Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (name && (*f))
+	(*f)(name);
+}
