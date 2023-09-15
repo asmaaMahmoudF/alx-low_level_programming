2bc79ce031310d84b659bc259a8ddee00a88246a
asmaaMahmoudFa <asmaafahmy932@gmail.com>
2023-09-14 19:05:36 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,21 +5,48 @@
 /**
 * print_all -  prints anything
 * @format: list of types of arguments passed to the function
-* Return: int
+* Return: void
 */
 
 void print_all(const char * const format, ...)
 {
 va_list ar;
-unsigned int i, sum = 0;
-if (n == 0)
-	return (0);
-va_start(ar, n);
-for (i = 0; i < n; i++)
-{
-sum += va_arg(ar, const unsigned int);
-}
+int n = 0, i = 0;
+	char *sep = ", ";
+	char *str;
+
+	va_start(ar, format);
+
+	while (format && format[i])
+		i++;
+
+	while (format && format[n])
+	{
+		if (n  == (i - 1))
+		{
+			sep = "";
+		}
+		switch (format[n])
+		{
+		case 'c':
+			printf("%c%s", va_arg(ar, int), sep);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(ar, int), sep);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(ar, double), sep);
+			break;
+		case 's':
+			str = va_arg(ar, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", str, sep);
+			break;
+		}
+		n++;
+	}
+printf("\n");
 va_end(ar);
-return (sum);
 }
 
