efe8c142e488f41f4625414780a7784f2fc5cb6c
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-10-25 00:29:50 +0300

    “listint_len”

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: data type pointer of struct
+ * Return: elements of the str i
+ */
+size_t listint_len(const listint_t *h)
+{
+    const listint_t *new_node = h;
+    size_t i = 0;
+    while (new_node)
+    {
+        new_node = new_node->next;
+        i++;
+    }
+    return (i);
+}
