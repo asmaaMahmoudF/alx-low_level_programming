8538d1296278f82f9e7da4c9081ee16b70bcf41a
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-10-24 23:09:56 +0300

    “asss”

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
 size_t print_listint(const listint_t *h)
 {
 	unsigned int i = 0;
-	const listint_t	 *new_node = h;
+	const listint_t *new_node = h;
 
-	while(h)
+	while (h)
 	{
 		printf("%d\n", new_node->n);
-		i++;
 		new_node = new_node->next;
+		i++;
 	}
 	return (i);
 }
-
