482ddd5b140204ece3618b7de2a59500b838d7ce
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-10-25 05:17:13 +0300

    “add_nodeint_end”

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
+ * add_nodeint_end - adds a new node at the beginning of a listint_t list
  * @head: data type pointer of struct
  * @n: valve of the new node
  * Return: the address of the new element, or NULL if it failed
