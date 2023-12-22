8639467869a87c9d30af55e9e45d3053d9fbf464
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-23 01:03:28 +0200

    task_0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,5 +6,16 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-
+	hash_table_t *new_table;
+	new_table = calloc(sizeof(hash_table_t));
+	if(!new_table)
+		return (0);
+	new_table->size = size;
+	new_table->array = calloc(sizeof(hash_node_t *) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (0);
+	}
+	return (new_table);
 }
