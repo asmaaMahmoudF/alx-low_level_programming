1629895ef06986cd53c30d364a664ed696ee95da
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-22 20:38:02 +0200

    task_0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+* hash_table_create - function that creates a hash table
+* @size : size of hash table
+* Return: pointer of hash_table_t
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
