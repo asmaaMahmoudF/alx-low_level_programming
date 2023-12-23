16ac9457cfd003dbdcec922b98fead2361280b63
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-23 04:15:11 +0200

    “task_3”

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,10 +2,9 @@
 /**
  * hash_table_print - function that retrieves a value associated with a key
  * @ht: is the hash table
- * @key:  is the key
- * Return: return a poiner of char(value)
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	
+
 }
