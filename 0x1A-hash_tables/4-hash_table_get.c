3b26382dec7d0680bb43282a46ed08c3104d49dd
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-23 04:34:43 +0200

    task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,16 +9,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *node;
+
 	if (!ht || !key)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
-	while(node != NULL)
-		{
-			if (strcmp(key, node->key) == 0)
-				return (node->value);
-			else
-			node = node->next;
-		}
+
+	while (node != NULL)
+	{
+		if (strcmp(key, node->key) == 0)
+			return (node->value);
+		node = node->next;
+	}
 	return (NULL);
 }
