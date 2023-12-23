3b8460d85e6a1ecbf8d8bd74b06dca5d0818b9af
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-23 04:50:16 +0200

    task_5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,5 +6,19 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+ if (!ht)  // Check if the hash table is NULL
+return;
 
+putchar('{');
+for (unsigned long int i = 0, comma = 0; i < ht->size; i++) {  // Iterate over the array
+hash_node_t *node = ht->array[i];
+while (node != NULL) {  // Traverse the linked list at this index
+if (comma)  // Print a comma before all but the first element
+printf(", ");
+printf("'%s': '%s'", node->key, node->value);  // Print the key/value pair
+node = node->next;  // Move to the next node
+comma = 1;  // After the first element, set comma to 1
+}
+}
+puts("}");
 }
