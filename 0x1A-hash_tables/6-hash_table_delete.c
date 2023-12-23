194c0b70f2f80237de1c66b65faf967b03babf30
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-23 04:46:09 +0200

    task_6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,5 +6,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *node;
+	hash_node_t *next_node;
+	unsigned long int i = 0;
 
+	if (!ht)
+		return (0);
+	while (ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			next_node = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = next_node;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
