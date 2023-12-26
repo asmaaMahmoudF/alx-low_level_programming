9546958411e7d2bce18aebe5d62f15e715544483
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-26 23:15:19 +0200

    task_0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_list - returns the number of elements in a linked list_t list
  * @h: input node list
@@ -8,6 +7,20 @@
  */
 size_t print_list(const list_t *h)
 {
+	unsigned int i = 0;
 
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("%s %d\n", h->str, h->len);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
-
